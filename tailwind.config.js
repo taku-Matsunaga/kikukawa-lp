52d86b609699e7536b60b1f16b21c304f475fafe
wcd-takuma-matsunaga <taku-matsunaga@wearcoord.com>
2024-02-03 13:15:11 +0900

    infoエリア

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      textBlack: '#1E3447',
+    },
     extend: {},
   },
   plugins: [require("tailgrids/plugin")],
